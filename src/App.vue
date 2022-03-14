9693191488f00c4c8ce65de0c74e4a6a2e044e2d
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-03-14 21:50:20 +0800

    💄 Styling

--- src/App.vue
+++ src/App.vue
@@ -116,7 +116,10 @@
             <div class="form_milestone_holder_wrap_outer shadow round-corner" style="width: 100vw">
               <div class="form_milestone_holder_wrap_inner">
                 <ul class="form_milestone_view" style="width: 100vw; display: flex">
-                  <li class="form_milestone_step green first" style="flex-grow: 26">
+                  <li
+                    class="form_milestone_step green first"
+                    style="flex-grow: 26; padding-left: 10px"
+                  >
                     <div class="floater green singleLine"></div>
                     <div class="content">
                       <div class="content_name">填写申请</div>
